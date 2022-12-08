aa1fcc2e09a760b74704464d72b7cc8e0f8cde03
Gabbeh <31376076+hvgab@users.noreply.github.com>
2022-12-08 17:18:45 +0100

    Move DEBUG to .env

--- get5django/get5django/settings.py
+++ get5django/get5django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ["localhost", "10.13.37.144"]
 
